dbfe09b35c274d9aa5c604133987e7ef784dcfe3
Fabian Kägy <mail@fabian-kaegy.de>
2021-07-22 21:33:38 +0200

    load styles everywhere

--- functions.php
+++ functions.php
@@ -14,12 +14,12 @@ add_action( 'init', __NAMESPACE__ . '\enqueue_frontend_assets' );
  */
 function enqueue_frontend_assets() {
 
-	if ( is_page_template( 'templates/van-pdf.php' ) ) {
+	// if ( is_page_template( 'templates/van-pdf.php' ) ) {
 		wp_enqueue_style(
 			'van-pdf-styles',
 			get_template_directory_uri() . '/templates/van-pdf.css',
 			[],
 			'1.0.0'
 		);
-	};
+	// };
 }
