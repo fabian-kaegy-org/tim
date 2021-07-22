3bebf43f6361e715898038534e233fe32c00b55f
Fabian Kägy <mail@fabian-kaegy.de>
2021-07-22 21:15:27 +0200

    add page template

---
+++ template-van-pdf.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: VAN PDF
+ *
+ * @package Tim
+ */
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div id="page" class="site">
+			<h1>Hello World!</h1>
+		</div>
+	</body>
+</html>
