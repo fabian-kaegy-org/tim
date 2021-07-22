827ce051d92895413abd68b06c2b10930f0569c5
Fabian Kägy <mail@fabian-kaegy.de>
2021-07-22 21:40:54 +0200

    move normalize into it's own stylesheet

--- functions.php
+++ functions.php
@@ -14,11 +14,18 @@ add_action( 'init', __NAMESPACE__ . '\enqueue_frontend_assets' );
  */
 function enqueue_frontend_assets() {
 
+	wp_register_style(
+		'normalize.css',
+		get_template_directory_uri() . '/normalize.css',
+		[],
+		'8.0.1'
+	);
+
 	// if ( is_page_template( 'templates/van-pdf.php' ) ) {
 		wp_enqueue_style(
 			'van-pdf-styles',
 			get_template_directory_uri() . '/templates/van-pdf.css',
-			[],
+			[ 'normalize.css' ],
 			'1.0.0'
 		);
 	// };
