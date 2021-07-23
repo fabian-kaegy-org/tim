726982b6825f884b4d25b5f29f5811e2985c47c7
Fabian Kägy <mail@fabian-kaegy.de>
2021-07-23 11:51:17 +0200

    remove jetpack styles

--- functions.php
+++ functions.php
@@ -9,6 +9,9 @@ namespace fabiankaegy\tim;
 
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_frontend_assets', 99 );
 
+add_filter( 'jetpack_sharing_counts', '__return_false', 99 );
+add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
+
 /**
  * Enqueue frontend assets
  */
