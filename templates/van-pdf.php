f9574a43159a2de65bae60cf253169395d51d45e
Fabian Kägy <mail@fabian-kaegy.de>
2021-07-23 09:53:13 +0200

    add width attribute to image

--- templates/van-pdf.php
+++ templates/van-pdf.php
@@ -21,14 +21,14 @@
 
 	<body <?php body_class(); ?>>
 		<div id="page" class="site">
-			<img class="hero-image" src="<?php echo esc_url( get_template_directory_uri() . '/images/van.jpg' ); ?>" alt="" />
+			<img width="800" class="hero-image" src="<?php echo esc_url( get_template_directory_uri() . '/images/van.jpg' ); ?>" alt="" />
 
 			<main class="site-content">
 				<h1 class="page-title"><span class="page-title__highlight">VWT5</span><br/>Buspläne</h1>
 
 				<p>Ich hoffe mit den folgenden Plänen und Materiallisten kann ich dir bei der Umsetzung deines Bus-Projekts helfen und dir einiges an Arbeit sparen!</p>
 
-				<img class="mockup" height="200" width="500" src="<?php echo esc_url( get_template_directory_uri() . '/images/van-pdf-mockup.png' ); ?>" alt="" />
+				<img class="mockup" height="450" src="<?php echo esc_url( get_template_directory_uri() . '/images/van-pdf-mockup.png' ); ?>" alt="" />
 
 				<a class="button__link" href="<?php echo esc_url( get_post_meta( get_the_ID(), 'pdf_url', true ) ); ?>" download target="_blank">Download</a>
 			</main>
